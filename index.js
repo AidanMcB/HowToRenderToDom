8ca976c329ebe28b824a10db6062b960137c0ba0
AidanMcB <aidankmcbride@gmail.com>
2020-08-07 17:12:25 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const header = document.createElement("h2")
+header.innerText = "i am a header"
+document.body.append(header)
+
+const button = document.createElement("button")
+button.innerText = "click me"
+document.body.append(button)
